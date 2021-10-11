bba10005da5a40d64b7c3189c33d37937315fbc3
MenachemMaron <65754196+MenachemMaron@users.noreply.github.com>
2021-10-11 06:27:32 +0300

    Delete IDM directory

--- IDM/js/content_script.js
+++
@@ -1,6 +0,0 @@
-(async() => {
-    const src = chrome.extension.getURL('js/background.js');
-    const contentScript = await
-    import (src);
-    contentScript.main( /* chrome: no need to pass it */ );
-})();
